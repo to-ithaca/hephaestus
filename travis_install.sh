36142e64b1dc5b0bf463ae43385f5797bc0b4fb3
Yilin Wei <yilin.wei10@gmail.com>
2017-04-23 17:40:15 +0100

    Create travis_install.sh

---
+++ travis_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GIT_URL=$0
+CLONE_DIR=$1
+VERSION=$2
+
+CURRENT_DIR=$(pwd)
+
+mkdir -p $HOME/.external
+cd $HOME/.external
+
+if[ ! -d "$CLONE_DIR" ]; then
+ git clone $GIT_URL $CLONE_DIR
+ cd $CLONE_DIR
+ git checkout -qf $VERSION
+else
+ cd $CLONE_DIR
+fi
+
+cmake -DBUILD_SHARED_LIBS=ON && make && sudo make install
+
+cd $CURRENT_DIR
